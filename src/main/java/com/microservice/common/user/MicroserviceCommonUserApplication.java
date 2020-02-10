5f4f5793e400c6ea1c47df34a94e4f7af57dc6c8
Adrian Ayarde <aayarde@overactive.com>
2020-02-10 16:14:49 -0300

    Project structure and model classes for microservices.

---
+++ src/main/java/com/microservice/common/user/MicroserviceCommonUserApplication.java
@@ -0,0 +1,11 @@
+package com.microservice.common.user;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class MicroserviceCommonUserApplication {
+
+}
